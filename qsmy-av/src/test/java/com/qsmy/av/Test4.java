5600b3e58f800698cc5acb72102544af4bf269d4
qsmy <qsmy0424@outlook.com>
2024-04-19 20:21:01 +0800

    commit

---
+++ qsmy-av/src/test/java/com/qsmy/av/Test4.java
@@ -0,0 +1,30 @@
+package com.qsmy.av;
+
+import cn.hutool.core.io.file.FileNameUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * @author qsmy
+ */
+@Slf4j
+public class Test4 {
+
+    public static void main(String[] args) throws IOException {
+        Files.walk(Paths.get("G:\\wwhm")).forEach(
+                path -> {
+                    if (!Files.isDirectory(path)) {
+                        try {
+                            System.out.println(FileNameUtil.getName(path.toString()) + "。" + path + "。" + Files.size(path) / 1024 / 1024);
+                        } catch (IOException e) {
+                            System.out.println(FileNameUtil.getName(path.toString()) + "。" + path + "。");
+                        }
+                    }
+                }
+        );
+    }
+}
